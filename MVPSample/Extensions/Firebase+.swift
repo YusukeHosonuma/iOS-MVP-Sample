91d53cbcf359806a93e4bc4a4d6f89bee6b2dd84
Yusuke Hosonuma <tobi462@gmail.com>
2018-02-18 19:25:29 +0900

    refactor: extract FirebaseModel

--- MVPSample/Extensions/Firebase+.swift
+++ MVPSample/Extensions/Firebase+.swift
@@ -10,8 +10,13 @@ import Foundation
 import FirebaseDatabase
 import CodableFirebase
 
+struct FirebaseModel<T> {
+    let key: String
+    let value: T
+}
+
 extension DatabaseReference {
-    func listen<T: Codable>(_ event: DataEventType, type: T.Type, callback: @escaping ([(key: String, value: T)]) -> ()) {
+    func listen<T: Codable>(_ event: DataEventType, type: T.Type, callback: @escaping ([FirebaseModel<T>]) -> ()) {
         self.observe(event) { (snapshot) in
             let items = snapshot.items(type)
             callback(items)
@@ -20,13 +25,13 @@ extension DatabaseReference {
 }
 
 extension DataSnapshot {
-    func items<T: Codable>(_ type: T.Type) -> [(key: String, value: T)] {
-        var xs: [(key: String, value: T)] = []
+    func items<T: Codable>(_ type: T.Type) -> [FirebaseModel<T>] {
+        var xs = [FirebaseModel<T>]()
         for child in self.children {
             let snapshot = child as! DataSnapshot
             do {
                 let value = try FirebaseDecoder().decode(type, from: snapshot.value!)
-                xs.append((key: snapshot.key, value))
+                xs.append(FirebaseModel(key: snapshot.key, value: value))
             } catch let error {
                 print(error) // TODO: KVS構造の変更には弱い（トレードオフ）
             }
