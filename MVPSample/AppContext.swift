39d56501a7a2b8ea7548e1d2320575762c95d22f
Yusuke Hosonuma <tobi462@gmail.com>
2018-02-18 19:36:23 +0900

    fix: TodoList to Singleton

---
+++ MVPSample/AppContext.swift
@@ -0,0 +1,18 @@
+//
+//  AppContext.swift
+//  MVPSample
+//
+//  Created by Yusuke Hosonuma on 2018/02/18.
+//  Copyright © 2018 Yusuke. All rights reserved.
+//
+
+import Foundation
+
+let appContext = AppContext()
+
+class AppContext {
+    
+    fileprivate init() {}
+    
+    let todoList = TodoList()
+}
