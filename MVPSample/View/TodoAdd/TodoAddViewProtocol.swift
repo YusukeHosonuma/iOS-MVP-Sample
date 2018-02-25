2becc4be09b44a281f260089b984482d4f4c6d18
Yusuke Hosonuma <tobi462@gmail.com>
2018-02-25 09:47:58 +0900

    refactor: extract file

---
+++ MVPSample/View/TodoAdd/TodoAddViewProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  TodoAddViewProtocol.swift
+//  MVPSample
+//
+//  Created by Yusuke Hosonuma on 2018/02/25.
+//  Copyright © 2018 Yusuke. All rights reserved.
+//
+
+import Foundation
+
+protocol TodoAddViewProtocol {
+    var titleText: String { get set }
+    func moveBack()
+}
